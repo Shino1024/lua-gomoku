b7e18c6cf682855e8362ed2f1dcfe67722f18570
Shino1024 <justincompspec@gmail.com>
2015-06-26 22:32:46 +0200

    getDataFromPlayers function is added.

--- main.lua
+++ main.lua
@@ -1,10 +1,13 @@
 local dim
 local table = {}
 local row = {}
+local player = {true, false}
+local chosenCords = {}
+
 function showTable(table, dim)
 	for a in 1, dim, 1 do
 		for b in 1, dim, 1 do
-			print '+-'
+			print "+-"
 		end
 		print '+'
 		for b in 1, dim, 1 do
@@ -14,7 +17,31 @@ function showTable(table, dim)
 		print '|'
 	end
 	for a in 1, dim, 1 do
-		print '+-'
+		print "+-"
 	end
 	print '+'
+end
+
+function getDataFromPlayers()
+	local answer
+	local coords = {}
+	repeat
+		if player[0] then
+			io.write("Player one, coordinates: ")
+		end
+		else
+			io.write("Player two, coordinates: ")
+		end
+		io.flush()
+		answer = io.read()
+		coords = string:match("([^,]+),([^,]+)")
+		if tonumber(coords[0]) ~= nil and tonumber(coords[1]) ~= nil then
+			if coords[0] > 0 and coords[0] <= dim and coords[1] > 0 and coords[1] <= dim then
+				chosenCords = coords
+			end
+		end
+		else
+			print "Enter correct coordinates!"
+		end
+	until true
 end
